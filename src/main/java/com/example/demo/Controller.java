9eb9e4857c36270a37860f3d86b495cd3fede0b1
Ray Chen <raymond.chen@mlb.com>
2019-05-09 16:25:13 -0400

    Added env logic

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @Value("${name}")
+    private String name;
+
     @GetMapping("/hello")
     public String handle() {
-        return "Hello WebFlux Blue";
+        return "Hello "+name;
     }
 }
