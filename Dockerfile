67449aae10e5056cc1a255c6bd0cf4227bed625e
Ray Chen <raymond.chen@mlb.com>
2019-05-10 21:52:11 -0400

    use env in deployment to control spring.profiles.active

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE
 COPY target/*.jar app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+# ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Dspring.profiles.active=$SPRING_PROFILE", "-jar","/app.jar"]
+ENTRYPOINT ["sh", "-c", "java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=$SPRING_PROFILE -jar /app.jar"]
--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,16 +1,32 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @Value("${name}")
     private String name;
 
+    @Value("${hello.first-name:}")
+    private String firstName;
+
+    @Value("${hello.last-name:}")
+    private String lastName;
+
+    @Value("${hello.env:qa}")
+    private String env;
+
     @GetMapping("/hello")
     public String handle() {
-        return "Hello "+name;
+        logger.info("Hey "+name+", env="+System.getenv("SPRING_PROFILE"));
+        logger.info("Hello "+name+": firstName="+firstName+", lastName="+lastName+", env"+env);
+
+        return "Hello "+name+": firstName="+firstName+", lastName="+lastName+", env"+env;
     }
 }
